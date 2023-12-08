3f52b18d0a4c55a63d4206ce1cbfbbbfd3fdd072
debfevlo <deborahfevlo@gmail.com>
2023-12-08 14:53:58 +0000

    added more js code to the project

--- src/index.js
+++ src/index.js
@@ -3,8 +3,44 @@ function displayTemperature(response){
     let temperatureElement= document.querySelector('#temperatures');
     let temperatureValue = response.data.temperature.current;
     temperatureElement.innerHTML = Math.round(temperatureValue);
+
     let h1 = document.querySelector('h1');
     h1.innerHTML = response.data.city;
+
+    let conditionElement = document.querySelector('#condition')
+    conditionElement.innerHTML = response.data.condition.description
+
+    let humidityElement = document.querySelector("#Humidity");
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+
+    let windyElement = document.querySelector("#windy");
+    windyElement.innerHTML = `${response.data.wind.speed}km/h`;
+
+    let timeElement = document.querySelector('#time');
+    let date = new Date(response.data.time * 1000);
+    timeElement.innerHTML = formatDate(date);
+
+    function formatDate(date){
+        let minute = date.getMinutes();
+        let hour = date.getHours();
+       let  days = [
+            'Sunday',
+            'Monday',
+            'Tuesday',
+            'Wednesday',
+            'Thursday',
+            'Friday',
+            'Saturday',
+
+         ]
+
+         if(minute > 10){
+            minute = `0${minute}`;
+         }
+          let day = days[date.getDay()];
+          return `${day} ${hour}:${minute}`;
+    }
+
     
 }
 
